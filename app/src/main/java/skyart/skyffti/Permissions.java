994299403f0e8b53c39c7817847f8657dbe39f82
TheSeeker427 <carr427b@gmail.com>
2017-02-17 22:20:44 -0500

    magic number.... whoops

--- app/src/main/java/skyart/skyffti/Permissions.java
+++ app/src/main/java/skyart/skyffti/Permissions.java
@@ -14,36 +14,15 @@ import android.support.v4.content.ContextCompat;
 public class Permissions {
 
 
+    public static int camera_code = 1; //main check
+    public static int internet_code = 2;
 
     public static void check(Activity activity, String permission){
         // do we have permission?
 
             ActivityCompat.requestPermissions(activity,
-                    new String[]{permission}, 1);
+                    new String[]{permission}, camera_code);
 
     }
 
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case 0: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
-    }
 }
